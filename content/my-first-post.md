121436ecb0584861b9ccfe0c929fd6f3ef6882c8
dasuttles <dasuttles@valdosta.edu>
2026-02-25 20:41:04 -0500

    Initial Pelican setup

---
+++ content/my-first-post.md
@@ -0,0 +1,6 @@
+Title: My First Post
+Date: 2026-02-25
+Tags: general
+Author: Dallas
+
+Hello world! This is my first NeoArchivist blog post.
---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+AUTHOR = 'Dallas A Suttles'
+SITENAME = 'NeoArchivist'
+SITEURL = ""
+
+THEME = 'themes/Flex'
+
+PATH = "content"
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Site basics
+SITESUBTITLE = "Archiving the 21st Century"
+
+# Profile image - we'll sort out where to put the file in a second
+SITELOGO = '/images/dasuttles_profile_2025.png'
+
+# Social links (delete any you don't want)
+SOCIAL = (
+    ("github", "https://github.com/dasuttles"),
+    ("twitter", "https://twitter.com/dasuttles"),
+)
+
+# Sidebar links
+LINKS = (
+    ("Valdosta State Archives", "https://www.valdosta.edu/archives"),
+)
+
+# Blogroll
+LINKS = (
+    ("Archives Indexes", "https://archives.valdosta.edu/indexes"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
